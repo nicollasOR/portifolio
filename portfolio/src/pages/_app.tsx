0eaab785eaf1beffdbb9d91c02074432c9830b51
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-06-03 17:47:30 -0300

    :lipstick:  feat: started components && header css :lipstick:

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,6 +1,39 @@
-import "@/styles/globals.css";
+
+import '../pages/styles/globals.css'
 import type { AppProps } from "next/app";
 
+import Head from "next/head";
+import Image from "next/image";
+import { M_PLUS_Rounded_1c, Lobster, Gajraj_One } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const gajraJ = Gajraj_One({
+  variable: "--font-header",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const RoundedM = M_PLUS_Rounded_1c({
+  variable: "--font-texto",
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "800", "900"],
+});
+
+const lobster = Lobster({
+  variable: "--font-titulo",
+  subsets: ["latin"],
+  weight: ["400"]
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <main className={`${gajraJ.variable} ${lobster.variable} ${RoundedM.variable}`}>
+  
+  
+  <Component {...pageProps} />
+  </main>
+  )
+
 }
+
+
---
+++ portfolio/src/pages/teste/index.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header/Header"
+
+
+const Teste = () => {
+    return (
+        <Header/>
+    )
+}
+
+export default Teste
